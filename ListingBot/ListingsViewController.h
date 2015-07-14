7b3603097afec71fa73ac7295524c6889a20423b
Andrew Robinson <SirArkimedes@gmail.com>
2015-07-14 14:03:41 -0500

    Change ViewController names

--- ListingBot/ListingsViewController.h
+++ ListingBot/ListingsViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ListingsViewController.h
 //  ListingBot
 //
 //  Created by Andrew Robinson on 7/14/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ListingsViewController : UIViewController
 
 @end
-
