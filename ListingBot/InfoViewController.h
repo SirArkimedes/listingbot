d262f797fe9e2a3486358f6a455ba8b719a6fa92
Andrew Robinson <SirArkimedes@gmail.com>
2015-07-14 20:42:45 -0500

    Begin First Launch Screens

---
+++ ListingBot/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  ListingBot
+//
+//  Created by Andrew Robinson on 7/14/15.
+//  Copyright (c) 2015 Robinson Bros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
