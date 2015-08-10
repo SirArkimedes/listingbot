0bd20583dc4fcf4da0453d3f4cbf826b3e5bb8df
Andrew Robinson <SirArkimedes@gmail.com>
2015-08-10 17:53:25 -0500

    Add share code page

---
+++ ListingBot/ShareListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareListViewController.h
+//  ListingBot
+//
+//  Created by Andrew Robinson on 8/10/15.
+//  Copyright (c) 2015 Robinson Bros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareListViewController : UIViewController
+
+@end
---
+++ ListingBot/ShareListViewController.m
@@ -0,0 +1,51 @@
+//
+//  ShareListViewController.m
+//  ListingBot
+//
+//  Created by Andrew Robinson on 8/10/15.
+//  Copyright (c) 2015 Robinson Bros. All rights reserved.
+//
+
+#import "ShareListViewController.h"
+
+@interface ShareListViewController ()
+
+@property (weak, nonatomic) IBOutlet UIButton *shareButton;
+
+@end
+
+@implementation ShareListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.shareButton.layer.cornerRadius = 5.0f;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Buttons
+
+- (IBAction)sharePress:(id)sender {
+    
+    UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+    pasteboard.string = @"listapp.com/122134234114321";
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
