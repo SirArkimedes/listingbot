b9674ed2bfa2c7121e2b4c522250c26a968288ac
SirArkimedes <sirarkimedes@gmail.com>
2015-08-12 16:02:16 -0500

    Redo list settings view

---
+++ ListingBot/ListSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ListSettingsViewController.h
+//  ListingBot
+//
+//  Created by Andrew Robinson on 8/12/15.
+//  Copyright (c) 2015 Robinson Bros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ListSettingsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ ListingBot/ListSettingsViewController.m
@@ -0,0 +1,66 @@
+//
+//  ListSettingsViewController.m
+//  ListingBot
+//
+//  Created by Andrew Robinson on 8/12/15.
+//  Copyright (c) 2015 Robinson Bros. All rights reserved.
+//
+
+#import "ListSettingsViewController.h"
+
+@interface ListSettingsViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation ListSettingsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"List Settings";
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"delete" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
